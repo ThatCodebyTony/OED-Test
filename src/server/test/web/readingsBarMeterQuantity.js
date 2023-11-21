94461a1ce95cd8db80deac48a2df949a4828611a
Tony <125552595+ThatCodebyTony@users.noreply.github.com>
2023-11-20 16:39:37 -0800

    Update readingsBarMeterQuantity.js
    Based on how everyone else committed, this is how the code should look. We were very close.

--- src/server/test/web/readingsBarMeterQuantity.js
+++ src/server/test/web/readingsBarMeterQuantity.js
@@ -124,19 +124,95 @@ mocha.describe('readings API', () => {
                 // Arthur: I think the "barWidthDays:" should be 1, if you look on the testing.md doc for B12, the description states 1 day bars for 15 minute intervals
                 // Arthur: If you go to test B1-B4 you can see the barWidthDays values correspond with the number stated in each of the descriptions 
                 
-               	mocha.it('B12: 75 day bars for 15 minute reading intervals and CO₂ units with +-inf start/end time & kg CO₂ as kg CO₂', async () => {
-                    // Load the data into the database
-
-		    // Tony: Changed the parameters to reflect that we're measuring CO₂.
-		    // These arrays can be found in src/server/util/readingsUtils.js
-                    await prepareTest(unitDataCO2, conversionDataCO2, meterDataCO2); 																	     
-
+                mocha.it('B12: 1 day bars for 15 minute reading intervals and quantity units with +-inf start/end time & kWh as kg of CO2', async () => {
+                    const unitData = [
+                        {
+                            // u2
+                            name: 'Electric_Utility',
+                            identifier: '',
+                            unitRepresent: Unit.unitRepresentType.QUANTITY,
+                            secInRate: 3600,
+                            typeOfUnit: Unit.unitType.METER,
+                            suffix: '',
+                            displayable: Unit.displayableType.NONE,
+                            preferredDisplay: false,
+                            note: 'special unit'
+                        },
+                        {
+                            // u10
+                            name: 'kg',
+                            identifier: '',
+                            unitRepresent: Unit.unitRepresentType.QUANTITY,
+                            secInRate: 3600,
+                            typeOfUnit: Unit.unitType.UNIT,
+                            suffix: '',
+                            displayable: Unit.displayableType.ALL,
+                            preferredDisplay: false,
+                            note: 'OED created standard unit'
+                        },
+                        {
+                            // u11
+                            name: 'metric ton',
+                            identifier: '',
+                            unitRepresent: Unit.unitRepresentType.QUANTITY,
+                            secInRate: 3600,
+                            typeOfUnit: Unit.unitType.UNIT,
+                            suffix: '',
+                            displayable: Unit.displayableType.ALL,
+                            preferredDisplay: false,
+                            note: 'OED created standard unit'
+                        },
+                        {
+                            // u12
+                            name: 'kg CO₂',
+                            identifier: '',
+                            unitRepresent: Unit.unitRepresentType.QUANTITY,
+                            secInRate: 3600,
+                            typeOfUnit: Unit.unitType.UNIT,
+                            suffix: 'CO₂',
+                            displayable: Unit.displayableType.ALL,
+                            preferredDisplay: false,
+                            note: 'special unit'
+                        }
+                    ];
+                    const conversionData = [
+                        {
+                            // c11
+                            sourceName: 'Electric_Utility',
+                            destinationName: 'kg CO₂',
+                            bidirectional: false,
+                            slope: 0.709,
+                            intercept: 0,
+                            note: 'Electric_Utility → kg CO₂'
+                        },
+                        {
+                            // c12
+                            sourceName: 'kg CO₂',
+                            destinationName: 'kg',
+                            bidirectional: false,
+                            slope: 1,
+                            intercept: 0,
+                            note: 'CO₂ → kg'
+                        }
+                    ];
+                    const meterData = [
+                        {
+                            name: 'Electric_Utility kg of CO₂',
+                            unit: 'Electric_Utility',
+                            displayable: true,
+                            gps: undefined,
+                            note: 'special meter',
+                            file: 'test/web/readingsData/readings_ri_15_days_75.csv',
+                            deleteFile: false,
+                            readingFrequency: '15 minutes',
+                            id: METER_ID
+                        }
+                    ];
+                    await prepareTest(unitData, conversionData, meterData); 																	     
                     // Get the unit ID since the DB could use any value.
-                    const unitId = await getUnitId('kg CO₂'); // Tony: changed to "kg CO₂'
+                    const unitId = await getUnitId('kg of CO₂');
                     // Load the expected response data from the corresponding csv file
-                    const unitId = await getUnitId('Electric_Utility');
                     // Load the expected response data from the corresponding csv file
-                    const expected = await parseExpectedCsv('src/server/test/web/readingsData/readings_ri_15_days_75.csv');
                     const expected = await parseExpectedCsv('src/server/test/web/readingsData/expected_bar_ri_15_mu_kWh_gu_kgCO2_st_-inf_et_inf_bd_1.csv');
                     // Create a request to the API with specific parameters for B12 and save the response
                     const res = await chai.request(app).get(`/api/unitReadings/bar/meters/${METER_ID}`)
